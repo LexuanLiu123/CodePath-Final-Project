53f59b7fe17e65178ab9354bb283f43dd1bc9bd0
SrikarPrayaga06 <27263Pwd>
2020-04-19 14:24:44 -0700

    login page

---
+++ Apartly/Apartly/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Apartly
+//
+//  Created by Srikar Prayaga on 4/19/20.
+//  Copyright © 2020 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
