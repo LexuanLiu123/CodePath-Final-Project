a0bae192a7530286d170beaa10675f7255728c2d
LexuanLiu123 <lel006@ucsd.edu>
2020-05-08 01:33:35 -0400

    Initial Commit

---
+++ Apartly/HomeScreenViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HomeScreenViewController.swift
+//  Apartly
+//
+//  Created by Judy Liu on 2020-05-08.
+//  Copyright © 2020 codepath. All rights reserved.
+//
+
+import UIKit
+
+class HomeScreenViewController: UIViewController {
+    
+    
+    @IBOutlet var readingView: UIImageView!
+    @IBOutlet var musicView: UIImageView!
+    @IBOutlet var homeworkView: UIImageView!
+    @IBOutlet var writingView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
