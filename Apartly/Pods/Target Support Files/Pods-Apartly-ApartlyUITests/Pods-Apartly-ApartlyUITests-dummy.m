ffad5bbf1fdfd04d2184d891be6d2fc12e5bc73a
Mubarek Mussa <mubarekmussa@Mubareks-MBP.san.rr.com>
2020-05-08 23:00:33 -0700

    check1

--- Apartly/Pods/Target Support Files/Pods-Apartly-ApartlyUITests/Pods-Apartly-ApartlyUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Apartly_ApartlyUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Apartly_ApartlyUITests
-@end
--- Apartly/Pods/Target Support Files/Pods-ApartlyTests/Pods-ApartlyTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ApartlyTests : NSObject
-@end
-@implementation PodsDummy_Pods_ApartlyTests
-@end
